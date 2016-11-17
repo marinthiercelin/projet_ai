eaa38c4b8df94e2ea8a59659b7cf7b34b772d6a2
marinthiercelin <marin.thiercelin@epfl.ch>
2016-11-17 12:42:23 -0500

    working deck

--- deck.py
+++ deck.py
@@ -1,12 +1,33 @@
+import itertools
+import random
+
+
+values = ["Ace","2","3","4",'5',"6","7","8","9","10","Jack","Queen","King"]
+suits = ["Spades","Diamonds","Hearts","Clubs"]
 #a deck of card
-class deck(self):
+class deck(object):
 	def __init__(self):
-		self.deck
+		self.ordered_list = list(itertools.product(values,suits))
+		self.deck = []
+		self.restart()
 		
 	def restart(self):
+		self.deck = list(self.ordered_list)
+		random.shuffle(self.deck)
 		
-	def get(self,number):
-		return n_card
-	
+	def get(self,number = 1):
+		result=[]
+		for n in xrange(number):
+			result.append(self.deck.pop())
+		return result
 	def burn(self,number):
-		burn_n
+		for n in xrange(number):
+			self.deck.pop()
+d = deck()
+print len(d.deck)
+d.burn(5)
+print len(d.deck)
+print d.get(5)
+print len(d.deck)
+d.restart()
+print len(d.deck)
