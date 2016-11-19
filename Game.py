241c246cc3b9d4e8d4aa8273b97880d589430c01
marinthiercelin <marin.thiercelin@epfl.ch>
2016-11-19 00:52:34 -0500

    modified game and added git ignore file

--- Game.py
+++ Game.py
@@ -1,6 +1,8 @@
 # game class
-import Deck
+from Deck import Deck
 from enum import Enum
+from random import randrange
+from player import player,action
 
 
 class Stage(Enum):
@@ -14,40 +16,66 @@ class Stage(Enum):
 class Game(object):
 	# Init takes an array of players and the number of players set to 1
 	# Blinds are the first chips to play the game
-	def __init__(self, players, blinds, num_players=2):
-		self.num_players = num_players
-		self.players = players
-		self.pot = 0
+	def __init__(self, player1, player2, blind, bet_value):
+		self.bet_value = bet_value
+		self.dealer = player1
+		self.small_blind = player2 
 		self.opened_cards = []
-		self.deck = Deck.Deck()
+		self.deck = Deck()
 		self.blinds = blinds
 		self.stage = Stage.preflop
 
 	def collect_blinds(self):
-		for i in xrange(self.num_players):
-			self.pot += 0  # self.players[i].collect_blind() #Needs to be implemented
+		self.small_blind.collect_money(blind/2)
+		self.dealer.collect_money(blind)
 
 	def deal(self):
 		self.deck.restart()
-		for i in xrange(self.num_players):
-			self.players[i].take_cards(self.deck.get(2))
+		self.small_blind.get_hand(deck.get(2))
+		self.dealer.get_hand(deck.get(2))
+	
+	def start_game(self):
+		while self.dealer.chips != 0 and self.small_blind.chips != 0:
+			self.stage = Stage.preflop
+			self.collect_blinds()
+			self.deal()
+			while not self.play_round()
+			self.end_of_round()#to implement
+			swap = self.dealer
+			self.dealer = self.small_blind
+			self.small_blind = swap
+		
 
 	#Collects bets and sends previous bets to current player
 	def collect_bets(self):
-		previous_bets = []
-		for i in xrange(self.num_players):
-			bet = self.players[i].get_bet(previous_bets)
-			previous_bets.append(bet)
-
-		for bet in previous_bets:
-			self.pot += bet
+		action1 = self.small_blind.play()#first small blind plays
+		if action1 = action.fold:
+			return False
+		self.small_blind.collect_money(abs(self.small_blind.on_table - self.dealer.on_table))#call
+		if action1 == action.bet:#then raise
+			self.small_blind.collect_money(self.bet_value)
+		
+		action2 = self.dealer.play(action1 != action.bet)#then dealer plays, he can raise if small blind didnt raise
+		if action2 = action.fold:
+			return False
+		self.d.collect_money(abs(self.small_blind.on_table - self.dealer.on_table))#call
+		if action2 == action.bet:
+			self.dealer.collect_money(self.bet_value)
+			action1 = self.small_blind.play(False)#if dealer raised, small blind either calls or fold
+			if action1 = action.fold:
+				return False
+			self.small_blind.collect_money(abs(self.small_blind.on_table - self.dealer.on_table))#call
+		return True # indicates that no one folded
+			
+			
 
 	def play_round(self):
 
-		self.collect_bets()
+		if not self.collect_bets() : return True #one of the player has folded
 
 		if self.stage is Stage.river: #River has been played, time to decide who won the game
 			self.stage = Stage.showdown
+			return True #end of the turn
 
 		elif self.stage is Stage.preflop: #Current Stage is Preflop so we open 3 cards
 			self.opened_cards += self.deck.get(3)
@@ -58,6 +86,7 @@ class Game(object):
 			self.opened_cards += self.deck.get()
 			self.deck.burn()
 			self.stage = Stage.turn if self.stage is Stage.flop else Stage.river
+		return False # turn is not over
 
 
 	def decide_winner(self):
--- player.py
+++ player.py
@@ -11,17 +11,36 @@ class player(object):
 		self.chips = chips
 		self.number = number
 		self.cards = []
+		self.money_on_table = 0
+		self.folded = False
+	
+	def collect_money(amount):
+		val =  max(0,self.chips-amount)
+		self.chips = val
+		self.money_on_table += val
+	
+	def win_money(amount):
+		self.chips += amount
 		
 	def new_hand(self, cards):
+		self.folded = False
+		self.money_on_table = 0
 		self.hand = cards
-		
-	def play(self):
-		move = raw_input("What do you do ? : 1 = call/check, 2 = bet, 3 = fold \n")
+	
+	def oponent_action(action):
+			
+	def play(self,can_call = True):
+		if can_call :
+			string = "What do you do ? : 1 = bet, 2 = fold, 3 = call/check \n"
+		else:
+			string = "What do you do ? : 1 = bet, 2 = fold\n"
+		move = raw_input(string)
 		if move == "1":
-			return action.call
-		elif move == "2" :
 			return action.bet
-		elif move == "3":
+		elif move == "2" :
+			self.folded = True
 			return action.fold
+		elif move == "3":
+			return action.call
 		else:
 			return self.play()
